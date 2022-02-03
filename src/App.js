9122d61872c355ed07b53f2dd2e94d8addf61ea2
Tuong Hoang The <tuong.hoang@tuni.fi>
2022-02-03 22:53:02 +0200

    Making header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { Header } from './Components/index';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
